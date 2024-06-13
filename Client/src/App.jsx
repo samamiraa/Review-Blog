91bdefa44c53ffd23f311cdc6085e31915fa04ff
Samantha Allen <samanthaamiraallen@gmail.com>
2024-06-13 11:11:05 -0300

    added header and footer template , material ui for blog

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
-  
+    <Header />
+    <Home />
+    <Footer />
     </>
   )
 }
